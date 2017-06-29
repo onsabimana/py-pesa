2e7144c94290ab1cacfb9933e14d9ea613b0f820
munemu82 <amos.munezero@atos.net>
2017-06-30 07:46:50 +1000

    Tanzania extracto changes

--- extractors/TanzaniaMPesaExtract.py
+++ extractors/TanzaniaMPesaExtract.py
@@ -19,13 +19,25 @@ def checkTypeOfSMS(word, str):
 def recievedMPesa(str):
 	if checkTypeOfSMS('received',str) ==True:		#check if this is for M-PESA recieved transaction
 		#1 perform extraction of reference number
-		refNo = example.partition(' ')[0]
+		refNo = str.partition(' ')[0]
 		print(refNo)
+		
 		#2 perform extraction of amount recieved
 		amountRecieved = re.findall('Tsh{1}[,0-9]{1,10}',str)
 		print(amountRecieved[0])
+		
 		#3 perform extraction of Sender's name 
+		nameString = re.search('from\s(.+?)\son', str)  #Extract the characters after "from" and space and before space and "on"
+		fullName = nameString.group(1)
+		firstName = fullName.partition(' ')[0];  	#Then extract only the first name from string which is the first
+		lastName = fullName.partition(' ')[2] 		#Then extract only the last name from string which is the first
+		print(fullName)
+		print(firstName)
+		print(lastName)
 		#4 perform extraction of date recieved
+		datestring = re.search('on\s(.+?)\s', str) 
+		timestring = re.search('at\s(.+?)\s', str) 
+		print(timestring.group(1))
 		#5 perform extraction of new balance (note that this could just be the current amount in the database + recieved in step 2 )
 	else:
 		print("Wrong function call!")
