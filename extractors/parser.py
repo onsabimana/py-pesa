51a8bba873d80edfbebbe4225e271c94572cf3f0
munemu82 <amos.munezero@atos.net>
2017-07-15 19:32:59 +1000

    New datetime extract function created

--- extractors/parser.py
+++ extractors/parser.py
@@ -1,4 +1,6 @@
 import re
+from dateutil.parser import parse
+import datetime
 
 example ='''Z10DN636 Confirmed.
 You have received Tsh50,000 from
@@ -19,9 +21,11 @@ New M-PESA balance is Tsh225.'''
 
 example6 = 'BA35RS735 Confirmed. on 8/3/14 at 4:59 PM Give Tsh10,000 cash to Hilda Joseph Mushi New M-PESA balance is Tsh10,015'
 
-#regex =r"([A-Z0-9]+\s+Confirmed.)|(received\s+Tsh\d+,\d+)|(from\s+(.+?)\s+?on)|(\d{2}\/\d\/\d{2}\s+at\s+\d:\d{2}\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+)"
-#matches = re.finditer(regex, example)
-
+example7 ='''BA35RT157 Confirmed.
+on 8/3/14 at 5:19 PM
+Withdraw Tsh6,000 from
+128137 - EMANUEL SHANI
+New M-PESA balance is Tsh3,415'''
 
 #-----------------------------------------------------START COMMON FUNCTIONS-----------------------------------------------------------------
 
@@ -31,11 +35,32 @@ def checkTypeOfSMS(word, str):
         return True
     else:
         return False
+#extract and format date in the string
+def extractDate(str):
+	words = str.split()
+	datepart =''
+	timepart =''
+	timepart2 =''
+	errorReturn='Error parsing date!'
+	for word in words:
+		if '/' in word:
+			datepart = word
+		elif ':' in word: 
+			timepart = word
+		elif (word=='PM') or (word=='AM') or (word=='PM.') or (word=='AM.'):
+			timepart2 = word
+	fulldate = datepart + ' ' + timepart+' '+timepart2
+	try:
+		fulldate = parse(fulldate,fuzzy=True)
+		fulldate = fulldate.strftime("%d-%m-%Y %H:%M:%S")
+	except ValueError:
+		return errorReturn
+	return fulldate
 #-----------------------------------------------------END COMMON FUNCTIONS-----------------------------------------------------------------
 
 #-----------------------------------------------------START SMS TRANSACTION FUNCTIONS-----------------------------------------------------------------
 
-def recievedMPesa(str):
+def computeTanzMPesa(str):
     lisfOfActualMatchs = [];
     transactionDetails = []
     regex = ''
@@ -62,16 +87,16 @@ def recievedMPesa(str):
 
     if checkTypeOfSMS('received',str) == True:       #check if this is for M-PESA recieved transaction
         regex =r"([A-Z0-9]+\s+Confirmed.)|(received\s+Tsh\d+,\d+|Tsh\d+)|(from\s+(.+?)\s+?on)|(\d{2}\/\d\/\d{2}\s+at\s+\d:\d{2}\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+|Tsh\d+)"
-    elif checkTypeOfSMS('fdhgfhgfhgfh',str) == True and len(str):
+    elif (checkTypeOfSMS('sent',str) ==True) and (checkTypeOfSMS('business',str) ==False):
         regex = r"([A-Z0-9]+\s+Confirmed.)|(\s+Tsh\d+,\d+|Tsh\d+)|(sent\s+(.+?)\s+?on)|(\d{2}\/\d{2}\/\d{2}\s+at\s+\d{2}:\d{2}\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+|Tsh\d+)"
-    elif checkTypeOfSMS('airtime on',str) == True and checkTypeOfSMS('bought',str):
+    elif (checkTypeOfSMS('airtime on',str) == True) and (checkTypeOfSMS('bought',str)==True):
         regex = r"([A-Z0-9]+\s+confirmed.|[A-Z0-9]+\s+Confirmed.)|(bought\s+Tsh\d+,\d+|Tsh\d+)|(from\s+(.+?)\s+?on)|(\d{2}\/\d\/\d{2}\s+at\s+\d:\d{2}\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+|Tsh\d+)"
-    elif checkTypeOfSMS('airtime for',str) == True  and checkTypeOfSMS('bought', str):
+    elif (checkTypeOfSMS('airtime for',str) == True) and (checkTypeOfSMS('bought', str)==True):
         regex=r"([A-Z0-9]+\s+confirmed.|[A-Z0-9]+\s+Confirmed.)|(bought\s+Tsh\d+,\d+|Tsh\d+)|(for\s+(.+?)\s+?on)|(\d{2}\/\d\/\d{2}|\d\/\d\/\d{2}\s+at\s+\d{2}:\d{2}\s+(PM|AM)|\s+\d:\d\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+|Tsh\d+)"
     elif checkTypeOfSMS('sent to business', str) == True:
-        regex = r"([A-Z0-9]+\s+confirmed.|[A-Z0-9]+\s+Confirmed.)|(Tsh\d+,\d+|Tsh\d+)|(for account\s+(.+?)\s+?on)|(\d{2}\/\d\/\d{2}\s+at\s+\d{2}:\d{2}\s+(PM|AM)|\d\/\d\/\d{2}\s+at\s+\d{2}:\d{2}\s+(PM|AM)|\s+\d:\d\s+(PM|AM))|(balance\s+is\s+Tsh\d+,\d+|balance\s+is\s+Tsh\d+)"
+        regex = r"([A-Z0-9]+\s+Confirmed.)|(Tsh\d+,\d+)|(for account\s+(.+?)\s+?on)|(business Bank)|(\d{2}\/\d\/\d{2}\s+at\s+\d:\d{2}\s+(PM|AM))|(balance\s+is\s+(Tsh\d+,\d+|Tsh\d+))"
     elif checkTypeOfSMS('Give',str) == True:
-        regex ="([A-Z0-9]+\s+confirmed.|[A-Z0-9]+\s+Confirmed.)|(on\s+\d{2}\/\d\/\d{2}\s+at\s+\d{2}:\d{2}\s+(PM|AM)|on\s+\d\/\d\/\d{2}\s+at\s+\d:\d{2}\s+PM|AM)"
+        regex = r"([A-Z0-9]+\s+Confirmed.)|(Give\s+Tsh\d+,\d+)|(to\s+(.+?)\s+?New)|(balance\s+is\s+(Tsh\d+,\d+|Tsh\d+))"
     else:
         print("Wrong function for this transaction!")
 
@@ -87,25 +112,22 @@ def recievedMPesa(str):
     if checkTypeOfSMS('received',str) ==True:
 
         # Extract transaction number
-        transactionNo =lisfOfActualMatchs[0].split()
-        transactionNo = transactionNo[0]
+        transactionNo =lisfOfActualMatchs[0].split()[0]
 
         # Extract Amount
-        amount = lisfOfActualMatchs[1].split()
-        amount = amount[1]
+        amount = lisfOfActualMatchs[1].split()[1]
 
         # Extract sender
         sender = lisfOfActualMatchs[2].split()
         sender = " ".join(sender[1:3])
 
         # Extract timestamp
-        timestamp = lisfOfActualMatchs[3].split()
-        timestamp = " ".join(timestamp[0] + timestamp[2:4])
+        timestamp = extractDate(str)
 
-        # Extract timestamp
+        # Extract balance
         balance = lisfOfActualMatchs[4].split()[2]
 
-    elif len(str) and checkTypeOfSMS('fsfdsfdsf', str):
+    elif (checkTypeOfSMS('sent',str) ==True) and (checkTypeOfSMS('business',str) ==False):
         # Extract transaction number
         transactionNo = lisfOfActualMatchs[0].split()[0]
 
@@ -117,77 +139,98 @@ def recievedMPesa(str):
         reciever = " ".join(reciever[2:4])
 
         # Extract timestamp
-        timestamp = lisfOfActualMatchs[3].split()
-        timestamp = " ".join(timestamp[0] + timestamp[2:4])
-
-        # Extract timestamp
-        balance = lisfOfActualMatchs[4].split()[2]
-
-    elif checkTypeOfSMS('airtime on', str) and checkTypeOfSMS('bought', str):
-        transactionAndAmountStr, _ , timestampStr, balanceStr = [elemet.split() for element in listOfActualMatchs]
-
-        # Extract transaction number
-        transactionNo, amount = transactionAndAmountStr[0], transactionAndAmountStr[1]
-
-        # Extract timestamp
-        timestamp = " ".join(timestamp[0] + timestamp[2:4])
+        timestamp = extractDate(str)
 
         # Extract balance
-        balance = balanceStr[2]
+        balance = lisfOfActualMatchs[4].split()[2]
 
-    elif checkTypeOfSMS('airtime for', str) and checkTypeOfSMS('bought', str):
+    elif (checkTypeOfSMS('airtime on',str) ==True) and (checkTypeOfSMS('bought',str)==True):
+        #transactionAndAmountStr, _ , timestampStr, balanceStr = [elemet.split() for element in listOfActualMatchs]
+
+        # Extract transaction number
+        transactionNo=lisfOfActualMatchs[0].split()[0]
+
+        # Extract Amount
+        amount = lisfOfActualMatchs[1].split()[1]
+		
+        # Extract timestamp
+        timestamp = extractDate(str)
+		
+        # Extract balance
+        balance = lisfOfActualMatchs[3].split()[2]
+
+    elif (checkTypeOfSMS('airtime for',str) ==True) and (checkTypeOfSMS('bought',str)==True):
         transactionNoStr, amountStr, receiverStr, timestampStr, balanceStr = [e.split() for e in lisfOfActualMatchs]
 
         # Extract transaction number
-        transactionNo = transactionStr[0]
+        transactionNo=lisfOfActualMatchs[0].split()[0]
 
         # Extract Amount
-        amount = transactionStr[1]
-
-        # amount = amount[1]
+        amount = lisfOfActualMatchs[1]
+		
         # Extract reciever
-        receiver = receiverStr[1]
+        reciever = lisfOfActualMatchs[2].split()[1]
 
         # Extract timestamp
-        timestamp = " ".join(timestampStr[0] + timestampStr[2: 3])
-
-        # Extract timestamp
-        balance = balanceStr[2]
+        timestamp = extractDate(str)
+		
+        # Extract balance
+        balance = lisfOfActualMatchs[4].split()[2]
 
     elif checkTypeOfSMS('sent to business', str):
-        transactionNo, amount, receiver, timestamp, balance = [e.split() for e in lisfOfActualMatchs]
-
-    elif checkTypeOfSMS('Give', str):
-        # Extract transaction number
+		#transactionNo, amount, receiver, timestamp, balance = [e.split() for e in lisfOfActualMatchs]
+		# Extract transaction number
         transactionNo=lisfOfActualMatchs[0].split()
         transactionNo = transactionNo[0]
-
+		
+		# Extract Amount
+        amount=lisfOfActualMatchs[1]
+		
+		# Extract sender
+        sender = lisfOfActualMatchs[3].split()[2]
+		
+		# Extract reciever
+        reciever = lisfOfActualMatchs[2]
+		
+		# Extract timestamp
+        timestamp = extractDate(str)
+		
+		# Extract balance
+        balance = lisfOfActualMatchs[4].split()[2]
+        
+    elif checkTypeOfSMS('Give', str):
+        # Extract transaction number
+        transactionNo=lisfOfActualMatchs[0].split()[0]
+       
         # Extract Amount
-        amount = re.findall('Give\s+Tsh{1}[,0-9]{1,10}',str)
-        amount=amount[0].split()  # this produces "Give Tsh78,878"
-        amount=amount[1]          # this extract "Tsh78,878"
+        amount=lisfOfActualMatchs[1].split()[1]
 
         # Extract reciever
         reciever = re.search('to\s(.+?)\sNew', str)  #Extract the characters after "to" and space and before space and "on"
         reciever = reciever.group(1)
 
         # Extract timestamp
-        timestamp = lisfOfActualMatchs[1]
+        timestamp = extractDate(str)
 
-        # timestamp = timestamp[0] + ' '+timestamp[2]+' '+timestamp[3]
-        # Extract timestamp
-        # balance = lisfOfActualMatchs[4]
-
+        # Extract balance
+        balance = lisfOfActualMatchs[3].split()[2]
+	elif checkTypeOfSMS('Withdraw',str) == True:
+		# Extract transaction number
+        transactionNo=lisfOfActualMatchs[0].split()[0]
+	
     #All the transactions details to the final list
     transactionDetails.append(transactionNo)    # add transaction number
 
     transactionDetails.append(amount)    # add amount
 
     transactionDetails.append(sender)    # add sender
+	
     transactionDetails.append(reciever)    # add reciever
+	
     transactionDetails.append(timestamp)    # add timestamp
+	
     transactionDetails.append(balance)    # add balance
 
     return transactionDetails
 
-print(recievedMPesa(example6))
+print(computeTanzMPesa(example7))
