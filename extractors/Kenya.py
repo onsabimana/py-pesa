339129504a2594a4c4a6ee24cdeeedc855a8841a
Amos Munezero <munemu82@gmail.com>
2017-08-12 17:37:51 +1000

    Kenya airtel private transactions complete
    Kenya airtel private transactions patterns complete

--- extractors/Kenya.py
+++ extractors/Kenya.py
@@ -1,12 +1,74 @@
+import re
 
-#----------------START OF AIRTEL TRANSACTION PATTERNS -------------------
+#----------------START OF KENYA AIRTEL TRANSACTION PATTERNS -------------------
 
-# Notification when buy Airtime
+# Notification when buy Airtime for yourself
 ke_airtel_buyairtime_money_notification_patterns = {
-    "transaction_id":  re.compile(r"Trans\s+\.\s+ID:\s+([A-Z0-9]+)"),
+    "transaction_id":  re.compile(r"Trans\.\s+ID:\s+([A-Z0-9]+)"),
     "sent_amount": None,
     "receiver_account": None,
-    "balance": re.compile(r"balance\s+is\s+(Tsh\d+,\d+|Tsh\d+)"),
-    "sender_account": re.compile(r"from\s+(.+?)\s+?on"),
-    "received_amount": re.compile(r"Airtime\s+of\s+(\d+,\d+|Tsh\d+)")
+    "balance": None,
+    "sender_account": None,
+    "received_amount": re.compile(r"Airtime\s+of\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)")
 }
+
+#Notification when a Person is sending you money.
+ke_airtel_received_money_notification_patterns = {
+    "transaction_id":  re.compile(r"Trans\.\s+ID:\s+([A-Z0-9]+)"),
+    "sent_amount": None,
+    "receiver_account": None,
+    "balance": re.compile(r"balance\s+is\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "sender_account": re.compile(r"from\s+(.+?)\."),
+    "received_amount": re.compile(r"received\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)")
+}
+
+#Notifications when you send another person money
+ke_airtel_send_money_notification_patterns = {
+    "transaction_id":  re.compile(r"Trans\.\s+ID:\s+([A-Z0-9]+)"),
+    "sent_amount": re.compile(r"sent\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "receiver_account": re.compile(r"to\s+(.+?)\."),
+    "balance": re.compile(r"balance\s+is\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "sender_account": None,
+    "received_amount": None
+}
+
+#Notification when you pay to a paybill number
+ke_airtel_paybill_money_notification_patterns = {
+    "transaction_id":  re.compile(r"Trans\.\s+ID:\s+([A-Z0-9]+)"),
+    "sent_amount": re.compile(r"sent\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "receiver_account": re.compile(r"to\s+(.+?)\s+?in\s+reference"),
+    "balance": re.compile(r"balance\s+is\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "sender_account": None,
+    "received_amount": None
+}
+
+#Notifications when depositing cash money into your account
+ke_airtel_cashdeposit_money_notification_patterns = {
+    "transaction_id":  re.compile(r"Trans\.\s+ID:\s+([A-Z0-9]+)"),
+    "sent_amount": None,
+    "receiver_account": None,
+    "balance": re.compile(r"balance\s+is\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "sender_account": re.compile(r"from\s+(.+?)\."),
+    "received_amount": re.compile(r"received\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)")
+}
+
+#Notification when you do a balance request - DO WE NEED THIS ONE !!
+ke_airtel_balancecheck_money_notification_patterns = {
+    "transaction_id": None,
+    "sent_amount": None,
+    "receiver_account": None,
+    "balance": re.compile(r"bal.\s+is\s+(Ksh\d+,\d+.\d+|Ksh\d+.\d+|Ksh\d+,\d+|Ksh\d+)"),
+    "sender_account": None,
+    "received_amount": None
+}
+
+#Notifications when an error occours. - NOT SURE IF WE NEED THIS, BUT MAYBE USEFUL FOR ANALYTICAL PURPOSES
+ke_airtel_error_money_notification_patterns = {
+    "transaction_id": None,
+    "sent_amount": re.compile(r"amount\s+of\s+(\d+,\d+.\d+Ksh|\d+.\d+Ksh|\d+,\d+Ksh|\d+Ksh)"),
+    "receiver_account": None,
+    "balance": None,
+    "sender_account": None,
+    "received_amount": None
+}
+#----------------END OF KENYA AIRTEL TRANSACTION PATTERNS -------------------
--- extractors/test.py
+++ extractors/test.py
@@ -8,7 +8,8 @@ import unittest
 from base_transaction_info import TransactionInfo
 
 import ghana
-import tanzaniaMpesa
+import tanzania
+import kenya
 
 
 class TestGhanaTransactionMessages(unittest.TestCase):
@@ -33,7 +34,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaRecievedMoneyNotification(self):
         test_message = 'Z10DN636 Confirmed. You have received Tsh50,000 from FREDRICK KIMARO on 27/1/14 at 1:19 PM New M-PESA balance is Tsh214,676'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_recieved_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_recieved_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "Z10DN636")
         self.assertEqual(info.received_amount, "Tsh50,000")
@@ -43,7 +44,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaSentMoneyNotification(self):
         test_message = 'X89IQ877 Confirmed. Tsh2,100 sent to MUSHI WILLIAM on 15/12/13 at 10:19 PM. New M-PESA balance is Tsh23,232.'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_sent_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_sent_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "X89IQ877")
         self.assertEqual(info.sent_amount, "Tsh2,100")
@@ -53,7 +54,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaBuyAirtimeMoneyNotification(self):
         test_message = 'Z92FP098 confirmed. You bought Tsh3,500 of airtime on 22/2/14 at 8:12 PM New M-PESA balance is Tsh2,720'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_buyairtime_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_buyairtime_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "Z92FP098")
         self.assertEqual(info.received_amount, "Tsh3,500")
@@ -62,7 +63,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaSendAirtimeMoneyNotification(self):
         test_message = 'Y24KU015 confirmed. You bought Tsh501 of airtime for 0765567959 on 1/1/14 at 10:09 PM New M-PESA balance is Tsh1,430'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_sendairtime_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_sendairtime_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "Y24KU015")
         self.assertEqual(info.sent_amount, "Tsh501")
@@ -72,7 +73,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaBankdepositMoneyNotification(self):
         test_message = 'Z15EE301 Confirmed. Tsh212,000 sent to business Bank for account ACB-11567341482 on 28/1/14 at 11:30 PM New M-PESA balance is Tsh225.'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_bankdeposit_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_bankdeposit_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "Z15EE301")
         self.assertEqual(info.sent_amount, "Tsh212,000")
@@ -82,7 +83,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesadepositMoneyNotification(self):
         test_message = 'BA35RS735 Confirmed. on 8/3/14 at 4:59 PM Give Tsh10,000 cash to Hilda Joseph Mushi. New M-PESA balance is Tsh10,015'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_deposit_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_deposit_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "BA35RS735")
         self.assertEqual(info.sent_amount, "Tsh10,000")
@@ -92,7 +93,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaWithdrawMoneyNotification(self):
         test_message = 'BA35RT157 Confirmed. on 8/3/14 at 5:19 PM Withdraw Tsh6,000 from 128137 - EMANUEL SHANI New M-PESA balance is Tsh3,415'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_withdraw_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_withdraw_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "BA35RT157")
         self.assertEqual(info.received_amount, "Tsh6,000")
@@ -102,7 +103,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzMpesaCheckBalanceMoneyNotification(self):
         test_message = 'BB43UB521 Confirmed. Your M-PESA balance was Tsh2,354 on 8/3/14 at 5:26 PM.'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_checkbalance_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_checkbalance_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "BB43UB521")
         self.assertEqual(info.balance, "Tsh2,354")
@@ -114,7 +115,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTztTigoRecievedMoneyNotification(self):
         test_message = 'New balance is Tsh 138,522. You have received Tsh 50,000 from CHARLES KOMBA, 0727666074. 31/01/2014 05:36 PM; with TxnId: PP141141.1843.D06413. Transact with...'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_recieved_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_recieved_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "PP141141.1843.D06413")
         self.assertEqual(info.received_amount, "Tsh 50,000")
@@ -124,7 +125,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoSentMoneyNotification(self):
         test_message = 'New balance is Tsh 2,515. Money sent successfully to ANJELA KIRIA, 0745239044. Amount: Tsh 31,000. Fee: Tsh 350.TxnID: PP134304.0912.J02744. You can now send...'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_sent_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_sent_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "PP134304.0912.J02744")
         self.assertEqual(info.sent_amount, "Tsh 31,000")
@@ -134,7 +135,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoBuyAirtimeMoneyNotification(self):
         test_message = 'New balance is Tsh 2,266. Your recharge request is successful for amount Tsh 3,501. TxnId : RC140320.1332.D09804. Transact with Tigo Pesa and win from Tshs...'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_buyairtime_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_buyairtime_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "RC140320.1332.D09804")
         self.assertEqual(info.received_amount, "Tsh 3,501")
@@ -143,7 +144,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoPayBillMoneyNotification(self):
         test_message = 'Bill Transaction has been sent to Tanesco.Please wait for confirmation TxnId: BP150143.1343.E03178, Bill Number:01343392423, transaction amount : 20,000 Tsh,new balance :71,073 Tsh, Company Tanesco.'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_mpesa_tigo_paybill_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_mpesa_tigo_paybill_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "BP150143.1343.E03178")
         self.assertEqual(info.sent_amount, "20,000 Tsh")
@@ -154,7 +155,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoBankdepositMoneyNotification(self):
         test_message = 'Bank payment successfull. The details are : TxnId: BP150121.1337.C09085, Ref Number:10411342553, transaction amount : 111,000 Tsh , charges: 0 Tsh,new balance :2,323 Tsh, Bank Name : ACB'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_bankdeposit_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_bankdeposit_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "BP150121.1337.C09085")
         self.assertEqual(info.sent_amount, "111,000 Tsh")
@@ -164,7 +165,7 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoDepositMoneyNotification(self):
         test_message = 'New balance is Tsh 35,165. Cash-In of Tsh 35,000 successful. Agent HILDA MUSHI. TxnID: CI154303.0933.G03265. You can now send money from your CRDB account to...'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_deposit_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_deposit_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "CI154303.0933.G03265")
         self.assertEqual(info.sent_amount, "Tsh 35,000")
@@ -174,12 +175,77 @@ class TestTanzaniaTransactionMessages(unittest.TestCase):
     def testTzTigoWithdrawMoneyNotification(self):
         test_message = 'New balance is Tsh 6,022. Cash-Out to EMANUEL KIULA was successful. Amount Tsh 130,000. Charges Tsh 2500.TxnID CO150121.3223.G00889. Transact with Tigo Pesa...'
 
-        info = TransactionInfo(test_message, tanzaniaMpesa.tz_tigo_withdraw_money_notification_patterns)
+        info = TransactionInfo(test_message, tanzania.tz_tigo_withdraw_money_notification_patterns)
 
         self.assertEqual(info.transaction_id, "CO150121.3223.G00889")
         self.assertEqual(info.received_amount, "Tsh 130,000")
         self.assertEqual(info.receiver_account, "EMANUEL KIULA")
         self.assertEqual(info.balance, "Tsh 6,022")
+
+    #-------------------- END OF TIGO TRANSACTIONS TESTS ----------------------
+class TestKenyaTransactionMessages(unittest.TestCase):
+    #-------------------- START OF KENYA AIRTEL PRIVATE TRANSACTIONS TESTS-------
+    def testKeAirtelBuyMoneyNotification(self):
+        test_message = 'Trans. ID: 7740224211672. You have received Airtime of 10.00Ksh from your OWN account.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_buyairtime_money_notification_patterns)
+
+        self.assertEqual(info.transaction_id, "7740224211672")
+        self.assertEqual(info.received_amount, "10.00Ksh")
+
+    def testKeAirtelReceiveMoneyNotification(self):
+        test_message = 'Trans. ID: 1440225031704 You have received 50.00Ksh from ANGELA NZIOKI. Your available balance is 155.00Ksh.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_received_money_notification_patterns)
+
+        self.assertEqual(info.transaction_id, "1440225031704")
+        self.assertEqual(info.received_amount, "50.00Ksh")
+        self.assertEqual(info.sender_account, "ANGELA NZIOKI")
+        self.assertEqual(info.balance, "155.00Ksh")
+
+    def testKeAirtelSendMoneyNotification(self):
+        test_message = 'Trans. ID: 1450335211705 You have sent 50.00Ksh to JOSEPH KARANJA. Your available balance is 10.00Ksh.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_send_money_notification_patterns)
+
+        self.assertEqual(info.transaction_id, "1450335211705")
+        self.assertEqual(info.sent_amount, "50.00Ksh")
+        self.assertEqual(info.receiver_account, "JOSEPH KARANJA")
+        self.assertEqual(info.balance, "10.00Ksh")
+
+    def testKeAirtelPayBillMoneyNotification(self):
+        test_message = 'Trans. ID: 2240545115618 You have sent 50.00Ksh to UHASIBU in reference to TEST. Your available balance is 105.00Ksh.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_paybill_money_notification_patterns)
+
+        self.assertEqual(info.transaction_id, "2240545115618")
+        self.assertEqual(info.sent_amount, "50.00Ksh")
+        self.assertEqual(info.receiver_account, "UHASIBU")
+        self.assertEqual(info.balance, "105.00Ksh")
+
+    def testKeAirtelCashDepositMoneyNotification(self):
+        test_message = 'Trans. ID: 0231412250320 You have received 200Ksh from ART111. Your available balance is 200.00Ksh.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_cashdeposit_money_notification_patterns)
+
+        self.assertEqual(info.transaction_id, "0231412250320")
+        self.assertEqual(info.received_amount, "200Ksh")
+        self.assertEqual(info.sender_account, "ART111")
+        self.assertEqual(info.balance, "200.00Ksh")
+
+    def testKeAirtelBalanceCheckMoneyNotification(self):
+        test_message = 'Your available bal. is Ksh60.00.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_balancecheck_money_notification_patterns)
+
+        self.assertEqual(info.balance, "Ksh60.00")
+
+    def testKeAirtelErrorMoneyNotification(self):
+        test_message = 'The amount you are trying to transfer is less than the minimum amount of 50.00Ksh allowed.'
+
+        info = TransactionInfo(test_message, kenya.ke_airtel_error_money_notification_patterns)
+
+        self.assertEqual(info.sent_amount, "50.00Ksh")
 
 if __name__ == '__main__':
     unittest.main()
